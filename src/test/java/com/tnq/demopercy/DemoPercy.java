0bece7cde345932351659893981db19d3131fd60
anandgit45 <anandtesting45@gmail.com>
2020-07-16 17:28:29 +0530

    New Code uploaded

---
+++ src/test/java/com/tnq/demopercy/DemoPercy.java
@@ -0,0 +1,22 @@
+package com.tnq.demopercy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoPercy {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().version("78").setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://practice.automationtesting.in/");
+
+		driver.findElement(By.xpath("//*[text()='My Account']")).click();
+
+	}
+
+}
